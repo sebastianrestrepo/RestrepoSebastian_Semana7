7d32d6a0760ace6d98fbbdc7560ac6911a88f78a
1144103496 <1144103496@sc309e14.academico.icesi.edu.co>
2017-03-03 15:43:40 -0500

    Se crean los objetos de tipo String

--- RestrepoSebastian_ProyectoIntegradorS7/src/Logica.java
+++ RestrepoSebastian_ProyectoIntegradorS7/src/Logica.java
@@ -3,7 +3,8 @@ import processing.core.PApplet;
 public class Logica {
 
 	private PApplet app;
-
+    private String palabras, colores, datos;
+    
 	public Logica(PApplet app) {
 		this.app = app;
 	}
--- RestrepoSebastian_ProyectoIntegradorS7/src/Palabra.java
+++ RestrepoSebastian_ProyectoIntegradorS7/src/Palabra.java
@@ -3,9 +3,13 @@ import processing.core.PApplet;
 public class Palabra {
 
 	private PApplet app;
+	private String nombres;
+	private String datos;
+	private String colores;
 
-	public Palabra(PApplet app) {
+	public Palabra(PApplet app, String nombres, String datos, String colores) {
 		this.app = app;
 	}
+	
 
 }
